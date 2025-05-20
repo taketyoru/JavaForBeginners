41a6466389b0e9959d7ee077a8bc87e6c5521a03
taketyoru <zeal24460033@fsgcl.jp>
2025-05-20 12:18:20 +0900

    3.1とちょっと3.2

---
+++ src/RectTest.java
@@ -0,0 +1,8 @@
+
+public class RectTest extends MyFrame {
+	public void run() {
+		fillRect(30,40,50,100);
+		fillRect(130,40,50,100);
+		fillRect(30,200,150,10);
+	}
+}
--- src/start.java
+++ src/start.java
@@ -3,7 +3,8 @@ public class start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
 	}
 
 }
