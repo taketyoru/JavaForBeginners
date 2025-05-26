999cb37d7a2d21e0ea20f41cf81b0f8358a82f99
taketyoru <zeal24460033@fsgcl.jp>
2025-05-26 10:05:10 +0900

    おそらく完成

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,8 +1,22 @@
 
 public class RectTest extends MyFrame {
 	public void run() {
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		
+		setColor(255,0,255);
 		fillRect(130,40,50,100);
+		
+		setColor(0,255,255);
 		fillRect(30,200,150,10);
+		
+		setColor(0,0,0);
+		fillRect(90,160,30,30);
+		
+		setColor(128,128,128);
+		fillRect(100,50,30,30);
+		
+		setColor(255,255,225);
+		fillRect(95,165,20,20);
 	}
 }
