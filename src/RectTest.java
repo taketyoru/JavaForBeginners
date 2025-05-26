e8965ad704c53652f5ea60b33c832efb2c5d58a6
taketyoru <zeal24460033@fsgcl.jp>
2025-05-26 10:17:38 +0900

    ちゃんとそろえた答え

--- src/RectTest.java
+++ src/RectTest.java
@@ -2,21 +2,21 @@
 public class RectTest extends MyFrame {
 	public void run() {
 		setColor(255,255,0);
-		fillRect(30,40,50,100);
+		fillRect(50,50,100,100);
 		
 		setColor(255,0,255);
-		fillRect(130,40,50,100);
+		fillRect(160,50,100,100);
 		
 		setColor(0,255,255);
-		fillRect(30,200,150,10);
+		fillRect(50,160,100,100);
 		
 		setColor(0,0,0);
-		fillRect(90,160,30,30);
+		fillRect(160,160,100,100);
 		
 		setColor(128,128,128);
-		fillRect(100,50,30,30);
+		fillRect(270,160,100,100);
 		
 		setColor(255,255,225);
-		fillRect(95,165,20,20);
+		fillRect(295,185,50,50); ///EX
 	}
 }
